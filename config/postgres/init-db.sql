54c899cc6d35094546b71aa6aafc982b6391ccb8
Simon Dietschi <dietschi.simon@gmail.com>
2026-01-05 03:25:43 +0100

    WIP

---
+++ config/postgres/init-db.sql
@@ -0,0 +1,20 @@
+-- Creates databases and enables pgvector extension for all services
+
+-- Enable pgvector extension on main database
+CREATE EXTENSION IF NOT EXISTS vector;
+
+-- Create additional databases for services
+CREATE DATABASE litellm_db;
+CREATE DATABASE openwebui_db;
+
+-- Connect to each database and enable pgvector
+
+\c litellm_db
+CREATE EXTENSION IF NOT EXISTS vector;
+
+\c openwebui_db
+CREATE EXTENSION IF NOT EXISTS vector;
+
+-- Grant permissions
+GRANT ALL PRIVILEGES ON DATABASE litellm_db TO aistack;
+GRANT ALL PRIVILEGES ON DATABASE openwebui_db TO aistack;
