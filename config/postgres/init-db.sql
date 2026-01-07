7b17917edc3de474d4556e6b230704ffa1b7ed36
Simon Dietschi <dietschi.simon@gmail.com>
2026-01-07 14:28:17 +0100

    MLflow

--- config/postgres/init-db.sql
+++ config/postgres/init-db.sql
@@ -6,6 +6,7 @@ CREATE EXTENSION IF NOT EXISTS vector;
 -- Create additional databases for services
 CREATE DATABASE litellm_db;
 CREATE DATABASE openwebui_db;
+CREATE DATABASE mlflow_db;
 
 -- Connect to each database and enable pgvector
 
@@ -18,3 +19,4 @@ CREATE EXTENSION IF NOT EXISTS vector;
 -- Grant permissions
 GRANT ALL PRIVILEGES ON DATABASE litellm_db TO aistack;
 GRANT ALL PRIVILEGES ON DATABASE openwebui_db TO aistack;
+GRANT ALL PRIVILEGES ON DATABASE mlflow_db TO aistack;
